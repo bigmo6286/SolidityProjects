cc02fd533d19df0646b503949215f63edc44eafb
bigmo6286 <141476575+bigmo6286@users.noreply.github.com>
2026-02-01 20:04:01 +0100

    Add files via upload

---
+++ StorageFactory.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import {SimpleStorage} from "./SimpleStorage.sol";
+
+contract StorageFactory{
+    SimpleStorage[] public listOfSimpleStorageContracts;
+    //address[] public listOfSimpleStorageContracts;
+
+    function createSimpleStorageContract() public {
+        SimpleStorage newSimpleStorageContract = new SimpleStorage();
+        listOfSimpleStorageContracts.push(newSimpleStorageContract);
+    }
+
+    function sfStore(uint256 _simpleStorageIndex, uint256 _newSimpleStorageNumber) public {
+        SimpleStorage mySimpleStorage = listOfSimpleStorageContracts[_simpleStorageIndex];
+        mySimpleStorage.store(_newSimpleStorageNumber);
+
+    }
+
+    function sfGet(uint256 _simpleStorageIndex) public view returns(uint256){
+       
+        return listOfSimpleStorageContracts[_simpleStorageIndex].retrieve();
+    }
+
+
+
+
+
+}
